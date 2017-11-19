eb56286a41cb54823db7e8f0ff094045d379d24b
ibsafa <isafa@wisc.edu>
2017-11-19 11:37:09 -0600

    Update nuFATE.h

--- c++/include/nuFATE/nuFATE.h
+++ c++/include/nuFATE/nuFATE.h
@@ -14,7 +14,8 @@
 #include <gsl/gsl_math.h>
 #include <gsl/gsl_blas.h>
 #include <gsl/gsl_eigen.h>
-
+#include <gsl/gsl_linalg.h>
+#include <gsl/gsl_permutation.h>
 
 #ifndef NUFATE_H
 #define NUFATE_H
@@ -22,48 +23,68 @@
 namespace nufate {
 
 struct Result {
-  std::shared_ptr<double> eval;
-  std::shared_ptr<double> evec;
-  std::shared_ptr<double> ci;
-  std::shared_ptr<double> energy_nodes;
-  std::shared_ptr<double> phi_0_;
+  double* eval;
+  double* evec;
+  double* ci;
+  std::vector<double> energy_nodes_;
+  std::vector<double> phi_0_;
 };
 
 class nuFACE {
   private:
-    const double GD = 1.;
+    double const GF = 1.16e-5;
+    double const hbarc = 1.97e-14;
+    double const GW = 2.085;
+    double const MW = 80.385e0;
+    double const mmu = 0.106e0;
+    double const me = 511.e-6;
+    double const pi = 3.14159265358979323846;
+    double const MZ = 91.18;
+    double const s2t = 0.23;
+    double const gL =  s2t-0.5;
+    double const gR = s2t;
   private:
     int newflavor_;
     double newgamma_;
     std::string newh5_filename_;
   public:
-    nuFACE(int flavor, double gamma, std::string h5_file);
+    nuFACE(int, double, std::string);
     Result get_eigs();
     int getFlavor() const;
     double getGamma() const;
     std::string getFilename() const;
   protected:
-    void set_glashow_total(unsigned int NumNodes, std::shared_ptr<double> energy_nodes);
-    std::shared_ptr<double> get_RHS_matrices(unsigned int NumNodes, std::shared_ptr<double> energy_nodes, std::shared_ptr<double> sigma_array_, std::shared_ptr<double> dxs_array_);
+    void set_glashow_total(unsigned int NumNodes_, std::vector<double> energy_nodes_);
+    void set_glashow_partial(unsigned int NumNodes_, std::vector<double> energy_nodes_);
+    void set_RHS_matrices(std::shared_ptr<double> RMatrix_, unsigned int NumNodes, std::vector<double> energy_nodes, std::vector<double> sigma_array_, std::shared_ptr<double> dxs_array_);
     double readDoubleAttribute(hid_t, std::string) const;
     unsigned int readUIntAttribute(hid_t, std::string) const;
-    std::shared_ptr<double> logspace(double min,double max,unsigned int samples) const;
+    std::vector<double> logspace(double min,double max,unsigned int samples) const;
   private:
-    unsigned int size_;
+    std::string grpdiff;
+    unsigned int NumNodes_;
+    double Emax_;
+    double Emin_;
     int dxsdim_[2];
-    std::shared_ptr<double> phi_0_;
-    std::shared_ptr<double> energy_nodes_;
-    std::shared_ptr<double> logpoints_;
-    std::shared_ptr<double> sigma_array_;
+    std::vector<double> energy_nodes_;
+    std::vector<double> sigma_array_;
+    std::vector<double> DeltaE_;
+    std::vector<double> phi_0_;
+    std::vector<double> glashow_total_;
     std::shared_ptr<double> dxs_array_;
     std::shared_ptr<double> tau_array_;
-    std::shared_ptr<double> DeltaE_;
     std::shared_ptr<double> RHSMatrix_;
     std::shared_ptr<double> RHregen_;
-    std::shared_ptr<double> glashow_total_;
-    unsigned int NumNodes_;
+    std::shared_ptr<double> glashow_partial_;
+    std::shared_ptr<double> Enuin_;
+    std::shared_ptr<double> Enu_;
+    std::shared_ptr<double> selectron_;
+    std::shared_ptr<double> den_;
+    std::shared_ptr<double> t1_;
+    std::shared_ptr<double> t2_;
+    std::shared_ptr<double> t3_;
+
 };
 
-} // close namespace
 
 #endif
