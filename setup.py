46d133556e0ac385707518b02878cab623669778
Ningqiang Song <nqsongnju@gmail.com>
2019-02-14 18:50:32 -0500

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
       description='Calculates relative attenuation of a neutrino flux through the Earth',
       long_description=long_description,
       packages=find_packages('./src/python'),
-      install_requires=['numpy', 'tables','scipy'],
-      author='Aaron Vincent, Ali Kheirandish, Carlos Arguelles, Ibrahim Safa',
+      install_requires=['numpy', 'tables','scipy','h5py'],
+      author='Aaron Vincent, Ali Kheirandish, Carlos Arguelles, Ibrahim Safa','Ningqiang Song',
       url='git@github.com:aaronvincent/nuFATE.git'
         )
